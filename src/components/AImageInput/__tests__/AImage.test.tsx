a720a4f3e2a6132dabd98669c93d7926259ac4a6
Uladzislau <u.yanusheuski@andersenlab.com>
2023-04-02 13:47:57 +0200

    add unit tests and fix elsin rules

---
+++ src/components/AImageInput/__tests__/AImage.test.tsx
@@ -0,0 +1,26 @@
+/** @format */
+import { AImageInput } from "../AImageInput";
+import { render, screen, fireEvent } from "@testing-library/react";
+
+describe("AImageInput", () => {
+	const onChangeMock = jest.fn();
+
+	afterEach(() => {
+		onChangeMock.mockReset();
+	});
+
+	it("renders the component with the correct title", () => {
+		render(<AImageInput name="test" value="test" onChange={onChangeMock} />);
+
+		expect(screen.getByText("Choose a profile picture:")).toBeInTheDocument();
+	});
+
+	it("displays the selected image", () => {
+		render(<AImageInput name="test" value="test" onChange={onChangeMock} />);
+
+		const file = new File(["test image file"], "test.png", { type: "image/png" });
+		fireEvent.change(screen.getByText("Choose a profile picture:"), { target: { files: [file] } });
+
+		expect(screen.getByAltText("test")).toBeInTheDocument();
+	});
+});
---
+++ src/routes/NotFoundPage/__tests__/NotFoundPage.test.tsx
@@ -0,0 +1,23 @@
+/** @format */
+import NotFoundPage from "../NotFoundPage";
+import { render, screen } from "@testing-library/react";
+import React from "react";
+
+describe("NotFoundPage", () => {
+	it("renders without crashing", () => {
+		render(<NotFoundPage />);
+	});
+
+	it("renders an image with a valid source and alt text", () => {
+		render(<NotFoundPage />);
+		const image = screen.getByAltText("notfound");
+		expect(image).toHaveAttribute("src", expect.stringContaining("404.png"));
+		expect(image).toHaveAttribute("alt", "notfound");
+	});
+
+	it("renders the image with a width of 500", () => {
+		render(<NotFoundPage />);
+		const image = screen.getByAltText("notfound");
+		expect(image).toHaveAttribute("width", "500");
+	});
+});
